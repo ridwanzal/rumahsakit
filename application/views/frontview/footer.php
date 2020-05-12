62892078c0177e0a285a7d4ff2181415050b2faa
ridwanzal <zalbinaridwan@gmail.com>
2020-05-13 05:48:12 +0700

    tawkto tidak tampil

--- application/views/frontview/footer.php
+++ application/views/frontview/footer.php
@@ -84,7 +84,7 @@
         imgviewer();
         fetchblog();
         lamar();
-        tawkto();
+        // tawkto();
         $('#search_submit').on('click', function(){
             let keyword = $('#search_input').val();
 
@@ -214,6 +214,17 @@
             }
         });
     }
+</script>
+<script type="text/javascript">
+    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+    (function(){
+    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+    s1.async=true;
+    s1.src='https://embed.tawk.to/5eb60609967ae56c5218227a/default';
+    s1.charset='UTF-8';
+    s1.setAttribute('crossorigin','*');
+    s0.parentNode.insertBefore(s1,s0);
+    })();
 </script>
 </body>
 
