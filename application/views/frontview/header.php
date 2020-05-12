0b9ff67044e41dfa5096859c409212252e7126eb
ridwanzal <zalbinaridwan@gmail.com>
2020-05-13 06:28:42 +0700

    tawk to problem

--- application/views/frontview/header.php
+++ application/views/frontview/header.php
@@ -44,17 +44,4 @@
     c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
     })(document);
     </script>
-        <!--Start of Tawk.to Script-->
-    <script type="text/javascript">
-    var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-    (function(){
-    var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-    s1.async=true;
-    s1.src='https://embed.tawk.to/5eb60609967ae56c5218227a/default';
-    s1.charset='UTF-8';
-    s1.setAttribute('crossorigin','*');
-    s0.parentNode.insertBefore(s1,s0);
-    })();
-    </script>
-    <!--End of Tawk.to Script-->
 </head>
