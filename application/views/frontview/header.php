a2ac0d4ad0147a5a6492c923206f4b77425a77c1
ridwanzal <zalbinaridwan@gmail.com>
2020-05-13 06:46:07 +0700

    tawk to problem

--- application/views/frontview/header.php
+++ application/views/frontview/header.php
@@ -44,4 +44,15 @@
     c.src='https://www.smartsuppchat.com/loader.js?';s.parentNode.insertBefore(c,s);
     })(document);
     </script>
+    
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=UA-166320634-1"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+
+    gtag('config', 'UA-166320634-1');
+    </script>
+
 </head>
