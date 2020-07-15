2ca21740b6997eeece5af4ffafe0a5a40c7bf502
root <zalbinaridwan@gmail.com>
2020-07-15 21:37:35 +0700

    routing add dokter list

--- application/config/routes.php
+++ application/config/routes.php
@@ -77,6 +77,7 @@ $route['profil/visimisi'] = 'main/visimisi';
 $route['profil/rekanan'] = 'main/rekanan';
 $route['profil/prestasi'] = 'main/prestasi';
 $route['profil/pmkp'] = 'main/pmkp';
+$route['profil/dokter'] = 'main/dokter';
 $route['artikel'] = 'main/artikel';
 $route['artikel/(:any)'] = 'blog/blog_content_detail/$1'; 
 $route['layanan/list/(:any)'] = 'layanan/layanan_detail/$1'; 
