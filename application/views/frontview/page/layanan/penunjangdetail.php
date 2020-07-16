4ec4f7b1364d6eb93d3590c3594ca97417eac34e
root <zalbinaridwan@gmail.com>
2020-07-16 11:02:39 +0700

    penunjang detail

--- application/views/frontview/page/layanan/penunjangdetail.php
+++ application/views/frontview/page/layanan/penunjangdetail.php
@@ -25,9 +25,9 @@
                         <br/>
                         <?php
                             if($row->thumb == '' || $row->thumb == NULL){ ?>
-                                <img src="<?php echo base_url()?>assets/penunjang/thumb.png" style="width:inherit;margin-bottom:0px;border-radius:10px;">
+                                <img src="<?php echo base_url()?>assets/layananpenunjang/thumb.png" style="width:inherit;margin-bottom:0px;border-radius:10px;">
                                 <?php }else{ ?>
-                                    <img src="<?php echo base_url()?>assets/penunjang/thumb/<?php echo $row->thumb;?>" style="width:200px;margin-bottom:0px;border-radius:10px;">
+                                    <img src="<?php echo base_url()?>assets/layananpenunjang/thumb/<?php echo $row->thumb;?>" style="width:200px;margin-bottom:0px;border-radius:10px;">
                                     <?php }
                         ?>
                         <div class="content" style="margin-top:30px;">
