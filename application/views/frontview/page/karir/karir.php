184c1054d8e61b64ccffed1ec7ba9b8a551d28a6
ridwanzal <zalbinaridwan@gmail.com>
2020-08-27 18:49:10 -0400

    karir wording change

--- application/views/frontview/page/karir/karir.php
+++ application/views/frontview/page/karir/karir.php
@@ -72,11 +72,7 @@
     <div class="container">
         <div class="row">
             <div class="col-lg-12 col-md-12 col-xs-12">
-                <h4>Berkembang bersama kami</h4>
-                <br/>
-                <p class="text_justify">
-                    Kami menawarkan pengalaman menarik dan jenjang karir untuk mengembangkan karir Anda. Dimana tempat bekerja (Lahan Ibadah) kami penuh dengan beragam pegawai yang ingin maju, semangat kerja keras, kreatif dan kemauan berkembang meng-upgrade diri dalam tiap tetes kontribusi yang di berikan. Di <b>RS AR-RASYID</b>, kami percaya bahwa setiap orang membawa talenta yang bernilai dalam memberikan pelayanan kepada setiap manusia. Jadi, apa yang Anda tunggu?
-                </p>
+                <h4>Informasi Lowongan Kerja</h4>
             </div>
         </div>
     </div>
@@ -86,7 +82,6 @@
     <div class="container">
         <div class="row">
                 <div class="col-xl-12 col-lg-12 col-md-12 col-sm-12 col-12">
-                                <h5 class="mb-0">Open Vacancy</h5>
                                 <br/>
                                 <div class="table-responsive">
                                     <table id="example" class="table table-striped table-bordered second" style="width:100%">
