172d99a95b42f37860f69ad254a3a9b360aff392
ridwanzal <zalbinaridwan@gmail.com>
2023-04-25 19:02:56 +0700

    update styling

--- application/views/frontview/page/index/section2.php
+++ application/views/frontview/page/index/section2.php
@@ -1,123 +1,127 @@
 <section class="section_two">
-    <div class="container">
-        <div class="row">
-            <div class="col-lg-12 col-md-12 col-xs-12">
-                <center><h4 class="section_two_head">Rumah Sakit Islam</h4></center>
-            </div>
-        </div>
-        <br/>
-        <br/>
-        <div class="row">
-            <div class="col-md-6">
-                <div class="card mb-4 ">
-                    <div class="card-body">
-                        <div class="row">
-                                <div class="col-lg-2 col-md-2 col-xs-12 img_bar">
-                                    <img src="https://image.flaticon.com/icons/svg/212/212295.svg" height="70px">
-                                </div>
-                                <div class="col-lg-10 col-md-10">
-                                    <h4>Rumah Sakit Islam</h4>
-                                    <p class="card-text">Rumah Sakit Islam Ar Rasyid merupakan salah satu
-                                                        Rumah Sakit Islam diPalembang, Sumatera Selatan yang
-                                                        bertujuan memberikan pelayanan kesehatan secara islami,
-                                                        modern dan profesional.</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                    </div>
-                                </div>
+   <div class="container">
+      <div class="row">
+         <div class="col-lg-12 col-md-12 col-xs-12">
+            <center>
+               <h4 class="section_two_head">Rumah Sakit Islam</h4>
+            </center>
+         </div>
+      </div>
+      <br />
+      <br />
+      <div class="row">
+         <div class="col-md-6">
+            <div class="card mb-4 ">
+               <div class="card-body">
+                  <div class="row">
+                     <div class="col-lg-2 col-md-2 col-xs-12 img_bar">
+                        <img src="<?= base_url();?>assets/frontview/img/ic1.svg" height="50px" style="opacity:.5;">
+                     </div>
+                     <div class="col-lg-10 col-md-10">
+                        <h4>Rumah Sakit Islam</h4>
+                        <p class="card-text">Rumah Sakit Islam Ar Rasyid merupakan salah satu
+                           Rumah Sakit Islam diPalembang, Sumatera Selatan yang
+                           bertujuan memberikan pelayanan kesehatan secara islami,
+                           modern dan profesional.</p>
+                        <div class="d-flex justify-content-between align-items-center">
                         </div>
-                    </div>
-                </div>
+                     </div>
+                  </div>
+               </div>
             </div>
-            <div class="col-md-6">
-                <div class="card mb-4 ">
-                    <div class="card-body">
-                        <div class="row">
-                                <div class="col-lg-2 col-md-2 col-xs-12 img_bar" >
-                                    <img src="https://image.flaticon.com/icons/svg/212/212289.svg" height="70px">
-                                </div>
-                                <div class="col-lg-10 col-md-10">
-                                    <h4>Tenaga Medis Berpengalaman</h4>
-                                        <p class="card-text">Tenaga medis Rumah Sakit Islam Ar Rasyid tentunya memiliki akhlak islami
-                                        dan kompeten dalam bidang nya sesuai dengan standar pelayanan Rumah Sakit Islam Ar rasyid.</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                    </div>
-                                </div>
+         </div>
+         <div class="col-md-6">
+            <div class="card mb-4 ">
+               <div class="card-body">
+                  <div class="row">
+                     <div class="col-lg-2 col-md-2 col-xs-12 img_bar">
+                        <img src="<?= base_url();?>assets/frontview/img/ic4.svg" height="50px" style="opacity:.5;">
+                     </div>
+                     <div class="col-lg-10 col-md-10">
+                        <h4>Tenaga Medis Berpengalaman</h4>
+                        <p class="card-text">Tenaga medis Rumah Sakit Islam Ar Rasyid tentunya memiliki akhlak islami
+                           dan kompeten dalam bidang nya sesuai dengan standar pelayanan Rumah Sakit Islam Ar rasyid.
+                        </p>
+                        <div class="d-flex justify-content-between align-items-center">
                         </div>
-                    </div>
-                </div>
+                     </div>
+                  </div>
+               </div>
             </div>
-            <div class="col-md-6">
-                <div class="card mb-4 ">
-                    <div class="card-body">
-                        <div class="row">
-                                <div class="col-lg-2 col-md-2 col-xs-12 img_bar">
-                                    <img src="https://image.flaticon.com/icons/svg/212/212298.svg" height="70px">
-                                </div>
-                                <div class="col-lg-10 col-md-10">
-                                    <h4>Pelayanan 24 Jam</h4>
-                                    <p class="card-text">Pelayanan yang disediakan 24 Jam dengan harapan kami
-                                                        menjadi Rumah Sakit pilihan yang memberikan pelayanan
-                                                        yang cepat, tepat, dan akurat.</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                    </div>
-                                </div>
+         </div>
+         <div class="col-md-6">
+            <div class="card mb-4 ">
+               <div class="card-body">
+                  <div class="row">
+                     <div class="col-lg-2 col-md-2 col-xs-12 img_bar">
+                        <img src="<?= base_url();?>assets/frontview/img/ic2.svg" height="50px" style="opacity:.5;">
+                     </div>
+                     <div class="col-lg-10 col-md-10">
+                        <h4>Pelayanan 24 Jam</h4>
+                        <p class="card-text">Pelayanan yang disediakan 24 Jam dengan harapan kami
+                           menjadi Rumah Sakit pilihan yang memberikan pelayanan
+                           yang cepat, tepat, dan akurat.</p>
+                        <div class="d-flex justify-content-between align-items-center">
                         </div>
-                    </div>
-                </div>
+                     </div>
+                  </div>
+               </div>
             </div>
-            <div class="col-md-6">
-                <div class="card mb-4 ">
-                    <div class="card-body">
-                        <div class="row">
-                            <div class="col-lg-2 col-md-2 col-xs-12 img_bar">
-                                <img src="https://image.flaticon.com/icons/svg/212/212286.svg" height="70px">
-                            </div>
-                            <div class="col-lg-10 col-md-10">
-                                <h4>Fasilitas yang memadai</h4>
-                                <p class="card-text">Dengan didukung fasilitas kesehatan yang memadai, untuk berbagai keperluan medis, baik fasilitas medis maupun fasilitas penunjang.</p>
-                                <div class="d-flex justify-content-between align-items-center">
-                                </div>
-                            </div>
+         </div>
+         <div class="col-md-6">
+            <div class="card mb-4 ">
+               <div class="card-body">
+                  <div class="row">
+                     <div class="col-lg-2 col-md-2 col-xs-12 img_bar">
+                        <img src="<?= base_url();?>assets/frontview/img/ic3.svg" height="50px" style="opacity:.5;">
+                     </div>
+                     <div class="col-lg-10 col-md-10">
+                        <h4>Fasilitas yang memadai</h4>
+                        <p class="card-text">Dengan didukung fasilitas kesehatan yang memadai, untuk berbagai keperluan
+                           medis, baik fasilitas medis maupun fasilitas penunjang.</p>
+                        <div class="d-flex justify-content-between align-items-center">
                         </div>
-                    </div>
-                </div>
+                     </div>
+                  </div>
+               </div>
             </div>
-            <div class="col-md-6">
-                <div class="card mb-4 ">
-                    <div class="card-body">
-                        <div class="row">
-                                <div class="col-lg-2 col-md-2 col-xs-12 img_bar">
-                                    <img src="https://image.flaticon.com/icons/svg/212/212322.svg" height="70px">
-                                </div>
-                                <div class="col-lg-10 col-md-10">
-                                    <h4>Peralatan yang Canggih</h4>
-                                    <p class="card-text">Peralatan kami lengkap, modern dan canggih untuk menunjang
-                                                         pelayanan yang optimal.</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                    </div>
-                                </div>
+         </div>
+         <div class="col-md-6">
+            <div class="card mb-4 ">
+               <div class="card-body">
+                  <div class="row">
+                     <div class="col-lg-2 col-md-2 col-xs-12 img_bar">
+                        <img src="<?= base_url();?>assets/frontview/img/ic6.svg" height="50px" style="opacity:.5;">
+                     </div>
+                     <div class="col-lg-10 col-md-10">
+                        <h4>Peralatan yang Canggih</h4>
+                        <p class="card-text">Peralatan kami lengkap, modern dan canggih untuk menunjang
+                           pelayanan yang optimal.</p>
+                        <div class="d-flex justify-content-between align-items-center">
                         </div>
-                    </div>
-                </div>
+                     </div>
+                  </div>
+               </div>
             </div>
-            <div class="col-md-6">
-                <div class="card mb-4 ">
-                    <div class="card-body">
-                        <div class="row">
-                                <div class="col-lg-2 col-md-2 col-xs-12 img_bar">
-                                    <img src="https://image.flaticon.com/icons/svg/240/240411.svg" height="70px">
-                                </div>
-                                <div class="col-lg-10 col-md-10">
-                                    <h4>Pelayanan yang memuaskan</h4>
-                                    <p class="card-text">Harapan besar Rumah Sakit Islam Ar Rasyid menjadi contoh
-Rumah Sakit Islam berbasis Islam di Indonesia.</p>
-                                    <div class="d-flex justify-content-between align-items-center">
-                                    </div>
-                                </div>
+         </div>
+         <div class="col-md-6">
+            <div class="card mb-4 ">
+               <div class="card-body">
+                  <div class="row">
+                     <div class="col-lg-2 col-md-2 col-xs-12 img_bar">
+                        <img src="<?= base_url();?>assets/frontview/img/ic5.svg" height="50px" style="opacity:.5;">
+                     </div>
+                     <div class="col-lg-10 col-md-10">
+                        <h4>Pelayanan yang memuaskan</h4>
+                        <p class="card-text">Harapan besar Rumah Sakit Islam Ar Rasyid menjadi contoh
+                           Rumah Sakit Islam berbasis Islam di Indonesia.</p>
+                        <div class="d-flex justify-content-between align-items-center">
                         </div>
-                    </div>
-                </div>
+                     </div>
+                  </div>
+               </div>
             </div>
-        </div>
-    </div>
+         </div>
+      </div>
+   </div>
 </section>
